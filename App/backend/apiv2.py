b9d39940b57d41d08035babac0265674499d4666
Sam Yip <sammomo@live.co.uk>
2022-08-30 01:37:01 +0100

    added api v2 router

---
+++ App/backend/apiv2.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi_pagination import add_pagination
+
+from apis.version2 import route_companies, route_jobs, route_recruiters
+
+COMPANY_ROUTER = "company"
+JOB_ROUTER = "job"
+RECRUITER_ROUTER = "recruiter"
+
+
+def add_routers(app_kwargs):
+    api = FastAPI(**app_kwargs)
+
+    routers = {
+        COMPANY_ROUTER: route_companies,
+        JOB_ROUTER: route_jobs,
+        RECRUITER_ROUTER: route_recruiters,
+    }
+
+    for name, router in routers.items():
+        add_pagination(router.router)
+        api.include_router(
+            router.router,
+            prefix=f"/{name}",
+            tags=[name],
+            responses={404: {"description": "Not Found"}},
+        )
+    return api
